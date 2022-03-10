a8a6ba0c973205b3d9d7ce48ecbe129417bc19f4
zenjutahi <zenjutahi@gmail.com>
2022-03-10 16:27:18 +0300

    learning jpa and mapping to database

---
+++ src/main/java/com/learing/jpa/concepts/javalearningjpa/JavaLearningJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learing.jpa.concepts.javalearningjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learing/jpa/concepts/javalearningjpa/JavaLearningJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learing.jpa.concepts.javalearningjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
